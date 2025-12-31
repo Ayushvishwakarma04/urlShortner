024e374ebe35900eb72556a000d044cb1d7682b8
Ayushvishwakarma04 <ayushvishwakarma9625@gmail.com>
2026-01-01 00:19:50 +0530

    Add rate limiting

--- routes/url.routes.js
+++ routes/url.routes.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import { handleGenerateNewShortURL,entryfunc, getAnalytics} from "../controller/url.controller.js";
+import { limiter } from "../controller/url.ratelimiter.js";
 const router=express.Router();
 
-router.post("/",handleGenerateNewShortURL);
+router.post("/",limiter,handleGenerateNewShortURL);
 export {router}
